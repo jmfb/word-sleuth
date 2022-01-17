e07bff4674b4b7049fe152fd77bdca9f896f9951
Jacob Buysse <Jacob.Buysse@gmail.com>
2022-01-17 09:34:17 -0600

    Ensure full 1s for invalid words and do a slow reveal for guesses.

--- client/src/pages/Home/Guess.tsx
+++ client/src/pages/Home/Guess.tsx
@@ -6,21 +6,26 @@ import styles from './Guess.css';
 export interface IGuessProps {
 	guess?: IGuess;
 	invalidWord?: boolean;
+	slowReveal?: number;
 }
 
 export default function Guess({
 	guess,
-	invalidWord
+	invalidWord,
+	slowReveal
 }: IGuessProps) {
 	const word = guess?.word ?? '     ';
 	const letterResults = guess?.letterResults ?? [];
+	const isRevealed = (index: number) => {
+		return slowReveal === undefined || index <= slowReveal;
+	};
 	return (
 		<div className={styles.root}>
 			{new Array(5).fill(0).map((value, index) =>
 				<Letter
 					key={index}
 					value={word[index]}
-					result={letterResults[index]}
+					result={isRevealed(index) ? letterResults[index] : undefined}
 					{...{invalidWord}}
 					/>
 			)}
--- client/src/pages/Home/NewGuess.tsx
+++ client/src/pages/Home/NewGuess.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useInterval } from '~/hooks';
 import Guess from './Guess';
 import { IGuess, IGuessResult, GuessStatus } from '~/models';
@@ -16,19 +16,28 @@ export default function NewGuess({
 	guess,
 	commitGuess
 }: INewGuessProps) {
+	const [counter, setCounter] = useState(0);
+
 	useInterval(() => {
 		if (guess) {
-			commitGuess();
+			if (counter === 5) {
+				commitGuess();
+				setCounter(0);
+			} else {
+				setCounter(counter + 1);
+			}
 		}
-	}, 1_000);
+	}, 200);
 
 	const word = (entry + '     ').substr(0, 5);
 	const currentGuess = { word, letterResults: null } as IGuess;
+	const slowReveal = guess ? counter : undefined;
 
 	return (
 		<Guess
 			guess={guess?.guess ?? currentGuess}
 			invalidWord={guess?.status === GuessStatus.InvalidWord}
+			{...{slowReveal}}
 			/>
 	);
 }
